9bf4d8d287c2c6919da736396929fca1aac3a247
kyle uyehara <kuyehara1@gmail.com>
2016-10-23 15:59:08 -1000

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// var ipInfo = $.getJSON('http://ipinfo.io', function loadIpInfo(ipInfo){
+//   return ipInfo;
+// });
+// var coord = ipInfo.loc.split(',');
+// console.log(coord);
+
+// var weatherData;
