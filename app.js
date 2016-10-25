43f450d5836269d5b6f7eb3f8c1e84836e9bf958
kyle uyehara <kuyehara1@gmail.com>
2016-10-24 23:38:52 -1000

    play with svg

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-// var ipInfo = $.getJSON('http://ipinfo.io', function loadIpInfo(ipInfo){
-//   return ipInfo;
-// });
-// var coord = ipInfo.loc.split(',');
-// console.log(coord);
 
-// var weatherData;
+var weatherData = {"coord":{"lon":139,"lat":35},
+"sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
+"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+"wind":{"speed":7.31,"deg":187.002},
+"rain":{"3h":0},
+"clouds":{"all":92},
+"dt":1369824698,
+"id":1851632,
+"name":"Shuzenji",
+"cod":200};
+
+
+
+
+
+
+
+
+
+
+
+
+// var ipData = $.getJSON('http://ipinfo.io', function loadIpInfo(ipData){
+//   return ipData;
+// });
+
+// var apiID = "5988ce438f59f323b9dae89d30886c85";
+
+// var ipDummyData = {
+//   "ip": "167.216.15.194",
+//   "hostname": "No Hostname",
+//   "city": "Honolulu",
+//   "region": "Hawaii",
+//   "country": "US",
+//   "loc": "21.3069,-157.8583",
+//   "org": "AS10348 High Technology Development Corporation",
+//   "postal": "96805"
+// };
