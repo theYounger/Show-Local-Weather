19996e165b42a543cad8e09e44f33324f6196ccf
kyle uyehara <kuyehara1@gmail.com>
2016-11-10 17:38:40 -1000

    implement api fetching and updating weather app accordingly

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
+$(document).ready(function() {
+  ASQ()
+  .then(function getIpInfo (done) {
+    $.get("http://ipinfo.io", function successIp(ipInfo) {
+      var coord = ipInfo.loc.split(",");
+      coord[0] = Math.floor(coord[0]);
+      coord[1] = Math.floor(coord[1]);
+      done(coord);
+    }, "jsonp");
+  })
+  .then(function getWeatherInfo(done, msg) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + msg[0] + "&lon=" + msg[1] + "&units=imperial&appid=5988ce438f59f323b9dae89d30886c85";
+    $.get(url, function successWeather(weatherInfo) {
+      done(weatherInfo);
+    }, "jsonp")
+  })
+  .then(function(done, msg) {
+    var weatherFeed = {};
 
-var weatherData = {"coord":{"lon":139,"lat":35},
-"sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-"wind":{"speed":7.31,"deg":187.002},
-"rain":{"3h":0},
-"clouds":{"all":92},
-"dt":1369824698,
-"id":1851632,
-"name":"Shuzenji",
-"cod":200};
-
-
-
-
-
-
-
-
-
-
-
-
-// var ipData = $.getJSON('http://ipinfo.io', function loadIpInfo(ipData){
-//   return ipData;
-// });
-
-// var apiID = "5988ce438f59f323b9dae89d30886c85";
-
-// var ipDummyData = {
-//   "ip": "167.216.15.194",
-//   "hostname": "No Hostname",
-//   "city": "Honolulu",
-//   "region": "Hawaii",
-//   "country": "US",
-//   "loc": "21.3069,-157.8583",
-//   "org": "AS10348 High Technology Development Corporation",
-//   "postal": "96805"
-// };
+    weatherFeed.city = msg.name;
+    weatherFeed.country = msg.sys.country;
+    weatherFeed.temp = msg.main.temp;
+    weatherFeed.conditions = msg.weather[0].main;
+    weatherFeed.iconUrl = "http://openweathermap.org/img/w/" + msg.weather[0].icon + ".png";
+    $("#weather-feed > h2").html(weatherFeed.city + ", " + weatherFeed.country);
+    $("#weather-feed > h3").html(weatherFeed.temp + " and " + weatherFeed.conditions)
+    .append("<img src=" + weatherFeed.iconUrl + " >");
+  });
+});
