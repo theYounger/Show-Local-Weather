240cabd78144a42c3683ab7e337ba3ec43578783
kyle uyehara <kuyehara1@gmail.com>
2016-11-10 18:05:01 -1000

    implement canvas for weather animation

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 $(document).ready(function() {
-  ASQ()
-  .then(function getIpInfo (done) {
-    $.get("http://ipinfo.io", function successIp(ipInfo) {
-      var coord = ipInfo.loc.split(",");
-      coord[0] = Math.floor(coord[0]);
-      coord[1] = Math.floor(coord[1]);
-      done(coord);
-    }, "jsonp");
-  })
-  .then(function getWeatherInfo(done, msg) {
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + msg[0] + "&lon=" + msg[1] + "&units=imperial&appid=5988ce438f59f323b9dae89d30886c85";
-    $.get(url, function successWeather(weatherInfo) {
-      done(weatherInfo);
-    }, "jsonp")
-  })
-  .then(function(done, msg) {
-    var weatherFeed = {};
+  // function draw() {
+  //   var ctx = (a canvas context);
+  //   ctx.canvas.width = window.innerWidth;
+  //   ctx.canvas.height = window.innerHeight;
+  // }
 
-    weatherFeed.city = msg.name;
-    weatherFeed.country = msg.sys.country;
-    weatherFeed.temp = msg.main.temp;
-    weatherFeed.conditions = msg.weather[0].main;
-    weatherFeed.iconUrl = "http://openweathermap.org/img/w/" + msg.weather[0].icon + ".png";
-    $("#weather-feed > h2").html(weatherFeed.city + ", " + weatherFeed.country);
-    $("#weather-feed > h3").html(weatherFeed.temp + " and " + weatherFeed.conditions)
-    .append("<img src=" + weatherFeed.iconUrl + " >");
-  });
+  // var canvas = $("#canvas");
+  // console.log(canvas);
+  // var ctx = canvas.getContext("2d");
+  // // ctx.fillStyle = "green";
+  // // ctx.fillRect(10, 10, 100, 100);
+  // console.log(ctx);
+//   ASQ()
+//   .then(function getIpInfo (done) {
+//     $.get("http://ipinfo.io", function successIp(ipInfo) {
+//       var coord = ipInfo.loc.split(",");
+//       coord[0] = Math.floor(coord[0]);
+//       coord[1] = Math.floor(coord[1]);
+//       done(coord);
+//     }, "jsonp");
+//   })
+//   .then(function getWeatherInfo(done, msg) {
+//     var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + msg[0] + "&lon=" + msg[1] + "&units=imperial&appid=5988ce438f59f323b9dae89d30886c85";
+//     $.get(url, function successWeather(weatherInfo) {
+//       done(weatherInfo);
+//     }, "jsonp")
+//   })
+//   .then(function(done, msg) {
+//     var weatherFeed = {};
+
+//     weatherFeed.city = msg.name;
+//     weatherFeed.country = msg.sys.country;
+//     weatherFeed.temp = msg.main.temp;
+//     weatherFeed.conditions = msg.weather[0].main;
+//     weatherFeed.iconUrl = "http://openweathermap.org/img/w/" + msg.weather[0].icon + ".png";
+//     $("#weather-feed > h2").html(weatherFeed.city + ", " + weatherFeed.country);
+//     $("#weather-feed > h3").html(weatherFeed.temp + " and " + weatherFeed.conditions)
+//     .append("<img src=" + weatherFeed.iconUrl + " >");
+//   });
 });
